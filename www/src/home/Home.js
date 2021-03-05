c477db6e17e4e48babbe44b8b14dae2079e90ee3
Soc Sieng <socsieng@google.com>
2021-03-05 14:22:55 -0800

    feat: update homepage text

--- www/src/home/Home.js
+++ www/src/home/Home.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import './Home.css';
 
 function Home() {
@@ -7,9 +8,20 @@ function Home() {
       <h1>Google Pay Loyalty API Demo</h1>
       <section className="content">
         <p>
-          This ample site is used to demonstrate how to integrate with the Google Pay Loyalty Enrollment and Sign-In
-          APIs with NodeJS and Firebase functions.
+          This ample site is used to demonstrate how to integrate with the Google Pay Loyalty APIs to save a loyalty
+          pass to Google Pay. This site will also handle Enrollment and Sign in request for a seamless sign up
+          experience when triggered from Google surfaces like Google Pay.
         </p>
+        <p>This site is built using React, Node.js, and uses Firebase Functions for hosting APIs.</p>
+        <p>Visit the loyalty pages:</p>
+        <ul>
+          <li>
+            <Link to="/sign-up">Loyalty sign-up</Link>
+          </li>
+          <li>
+            <Link to="/sign-in">Loyalty sign-in</Link>
+          </li>
+        </ul>
       </section>
     </div>
   );
